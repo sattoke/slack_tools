0a7db80f94e5c6ef675cb268dbb904a4c85059f2
Satoshi Yagi <git@toke.jp>
2020-04-20 01:13:07 +0900

    add list_files and delete_files feature

---
+++ delete_files.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Tool for deliting the uploaded files
+
+This tool uses `python-slackclient`.
+https://github.com/slackapi/python-slackclient
+"""
+
+import argparse
+import json
+import os
+import sys
+
+import slack
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--list-file", default="-", help="list of file IDs to be deleted.")
+
+    args = parser.parse_args()
+
+    try:
+        token = os.environ["SLACK_API_TOKEN"]
+    except KeyError:
+        print("The environment variable `SLACK_API_TOKEN` was not found. "
+              "Set your Slack API token to the enviroment variable `SLACK_API_TOKEN` "
+              "before running this tool by executing the following :\n"
+              "    $ read -sp 'Input your Slack API token: ' SLACK_API_TOKEN; echo && export SLACK_API_TOKEN")
+        exit(1)
+
+    client = slack.WebClient(token=os.environ["SLACK_API_TOKEN"])
+
+    def delete_from(list_file):
+        for line in list_file:
+            file_id = line.split()[0]
+
+            resp = client.files_delete(file=file_id)
+            print(json.dumps(resp.data, ensure_ascii=False))
+
+    if args.list_file == "-":
+        delete_from(sys.stdin)
+    else:
+        with open(args.list_file, mode='r') as f:
+            delete_from(f)
---
+++ list_files.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Tool for listing the uploaded files
+
+This tool uses `python-slackclient`.
+https://github.com/slackapi/python-slackclient
+"""
+
+import argparse
+import json
+import os
+
+import slack
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--raw-output", action='store_true', help="Output a raw slack API response.")
+    parser.add_argument("-u", "--user-id", help="Filter files created by a single user.")
+
+    args = parser.parse_args()
+
+    try:
+        token = os.environ["SLACK_API_TOKEN"]
+    except KeyError:
+        print("The environment variable `SLACK_API_TOKEN` was not found. "
+              "Set your Slack API token to the enviroment variable `SLACK_API_TOKEN` "
+              "before running this tool by executing the following :\n"
+              "    $ read -sp 'Input your Slack API token: ' SLACK_API_TOKEN; echo && export SLACK_API_TOKEN")
+        exit(1)
+
+    client = slack.WebClient(token=os.environ["SLACK_API_TOKEN"])
+
+    api_args = {}
+
+    if args.user_id:
+        api_args["user"] = args.user_id
+
+    # In my environment, the pagination function of SlackResponse didn't work properly,
+    # so I implemented it as follows in this program
+    page = 0  # page is one-based indexing.
+    while True:
+        page += 1
+        api_args["page"] = page
+
+        resp = client.files_list(**api_args)
+
+        if args.raw_output:
+            print(json.dumps(resp.data, ensure_ascii=False))
+        else:
+            for f in resp["files"]:
+                print(f'{f["id"]}\t{f["user"]}\t{f["is_public"]}\t{f["name"]}"')
+
+        if resp.data["paging"]["pages"] == resp.data["paging"]["page"]:
+            break
